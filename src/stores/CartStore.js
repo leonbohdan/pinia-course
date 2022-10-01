c0d86a7923eebc90e907b01f3958f728fe7bbc3a
Bohdan Leonets <bohdan@mytowerapp.com>
2022-10-01 23:43:17 +0300

    Dynamic Getters and Displaying the Cart Items

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { groupBy } from "lodash";
 
 export const useCartStore = defineStore("CartStore", {
     state: () => {
@@ -9,8 +10,9 @@ export const useCartStore = defineStore("CartStore", {
 
     getters: {
         count: (state) => state.items.length,
-
         isEmpty: (state) => state.count === 0,
+        grouped: (state) => groupBy(state.items, item => item.name),
+        groupCount: (state) => (name) => state.grouped[name].length,
     },
 
     actions: {
