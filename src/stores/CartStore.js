5f52b59cac43f6daf2266954ec4614dec5ef435f
Bohdan Leonets <bohdan@mytowerapp.com>
2022-10-02 16:04:15 +0300

    Preserve State with Hot Module Replacement

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,4 @@
-import { defineStore } from "pinia";
+import { defineStore, acceptHMRUpdate } from "pinia";
 import { groupBy } from "lodash";
 import { useAuthUserStore } from "./AuthUserStore.js";
 
@@ -48,3 +48,8 @@ export const useCartStore = defineStore("CartStore", {
         }
     }
 });
+
+// make sure to pass the right store definition, `useAuth` in this case.
+if (import.meta.hot) {
+    import.meta.hot.accept(acceptHMRUpdate(useCartStore, import.meta.hot))
+}
