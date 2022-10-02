26e3b8899e570255df30feeb443ca37db62867ad
Bohdan Leonets <bohdan@mytowerapp.com>
2022-10-02 17:20:31 +0300

    Subscribe to the State

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref, reactive } from "vue";
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 
@@ -7,6 +8,35 @@ import { useCartStore } from "@/stores/CartStore.js";
 const productStore = useProductStore();
 const cartStore = useCartStore();
 productStore.fill();
+
+const history = reactive([]);
+const future = reactive([]);
+const doingHistory = ref(false);
+history.push(JSON.stringify(cartStore.$state));
+
+const redo = () => {
+  const latestState = future.pop();
+  if (!latestState) return;
+  doingHistory.value = true;
+  history.push(latestState);
+  cartStore.$state = JSON.parse(latestState);
+  doingHistory.value = false;
+};
+
+const undo = () => {
+  if (history.length === 1) return;
+  doingHistory.value = true;
+  future.push(history.pop());
+  cartStore.$state = JSON.parse(history.at(-1));
+  doingHistory.value = false;
+};
+
+cartStore.$subscribe((mutation, state) => {
+  if (!doingHistory.value) {
+    history.push(JSON.stringify(state));
+    future.splice(0, future.length);
+  }
+});
 
 const unsubscribe = cartStore.$onAction(
   ({
@@ -35,6 +65,12 @@ const unsubscribe = cartStore.$onAction(
 <template>
   <div class="container">
     <TheHeader />
+
+    <div class="mb-5 flex justify-end">
+      <AppButton @click="undo">Undo</AppButton>
+      <AppButton class="ml-2" @click="redo">Redo</AppButton>
+    </div>
+
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in productStore.products"
