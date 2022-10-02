4401484fc18db5b3d21498c7547559993c1e65db
Bohdan Leonets <bohdan@mytowerapp.com>
2022-10-02 15:36:18 +0300

    Access Pinia State in the Options API

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,29 @@
-<script setup>
-// imports
+<script>
+import { useAuthUserStore } from "@/stores/AuthUserStore.js";
+import { mapState } from "pinia";
 import CartWidget from './CartWidget.vue';
+
+export default {
+  components: {
+    CartWidget,
+  },
+
+  computed: {
+    ...mapState(useAuthUserStore, ["username"]),
+  },
+};
 </script>
 
 <template>
-  <header 
+  <header
     class="flex justify-between p-6 mb-10 items-center"
     style="background-image: url('/images/double-bubble-outline.png')"
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
-    <CartWidget/>
+
+    <div>
+      <span class="mr-5">{{ username }}</span>
+      <CartWidget class="inline-block"/>
+    </div>
   </header>
 </template>
